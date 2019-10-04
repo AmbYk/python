520befb9c96ad542b552279b0a944239e5d17d91
AmbYk <56157293+AmbYk@users.noreply.github.com>
2019-10-04 13:32:13 +0200

    Delete Random1.py

--- Random1.py
+++
@@ -1,8 +0,0 @@
-# Vytvoril Andrej Ambroz z V1C
-
-import random
-spodniHranice = int(input("Vyberte spodni hranici nahodneho cisla: "))
-horniHranice = int(input("Vyberte horni hranici nahodneho cisla: "))
-
-print("")
-print("Nahodne cislo od", spodniHranice, "do", horniHranice, "=", random.randrange(spodniHranice, horniHranice))
